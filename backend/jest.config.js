f1482c161cc47bd526ee93a5c34b98bac6e0e90e
unknown <kamilnajem@gmail.com>
2026-01-29 00:16:48 +0200

    test

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
-  },
+  }, 
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/**/*.d.ts',
